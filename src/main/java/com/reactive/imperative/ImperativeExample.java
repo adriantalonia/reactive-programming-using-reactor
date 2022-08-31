961ea4e97f5fbe3f2a7e6b05714660d4e4b596de
Adrian <adrian.talonia11@gmail.com>
2022-08-30 23:05:45 -0500

    imperative example

---
+++ src/main/java/com/reactive/imperative/ImperativeExample.java
@@ -0,0 +1,23 @@
+package com.reactive.imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImperativeExample {
+    public static void main(String[] args) {
+        var names = List.of("alex", "ben", "chole", "afam");
+        var newNames = namesGreaterTanSize(names, 3);
+        System.out.println("new names list: "+newNames);
+    }
+
+    private static List<String> namesGreaterTanSize(List<String> names, int i) {
+        var newList = new ArrayList<String>();
+
+        for (String name : names) {
+            if (name.length() > 3) {
+                newList.add(name);
+            }
+        }
+        return newList;
+    }
+}
